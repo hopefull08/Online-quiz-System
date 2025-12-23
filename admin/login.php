cae49e84fa754708aebbad7f371faf450eea8156
hopefull08@gmail.com <netsanet.fentahun.8.13@gmail.com>
2025-12-23 20:52:05 +0400

    login page

---
+++ admin/login.php
@@ -0,0 +1,38 @@
+<?php
+include '../includes/db.php';
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM admins WHERE username='$username'");
+    if ($result && $result->num_rows > 0) {
+        $admin = $result->fetch_assoc();
+        if (password_verify($password, $admin['password'])) {
+            $_SESSION['admin_id'] = $admin['id'];
+            header("Location: qlist.php");
+            exit;
+        } else {
+            $error = "Invalid password.";
+        }
+    } else {
+        $error = "User not found.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login</title>
+</head>
+<body>
+    <h1>Admin Login</h1>
+    <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <form method="post">
+        <label>Username: <input type="text" name="username"></label><br>
+        <label>Password: <input type="password" name="password"></label><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
